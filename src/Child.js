7f39fcbedf9d2e8048f7f84aaa7d12f57cf3eca1
farjana islam <nipa.farzana87@gmail.com>
2022-12-12 19:04:05 +0600

    dsfsdf

--- src/Child.js
+++ src/Child.js
@@ -1,17 +1,24 @@
-import React, { useContext } from 'react';
-import { COUNTER_CONTEXT } from './App';
+import React, { useReducer } from 'react';
 
 const Child = () => {
-  const {count, setCount} = useContext(COUNTER_CONTEXT);
+  const initialState = 0;
+  const reducer = (state, action) => {
+    if(action.type === "INCREMENT"){
+      return state + action.payload;
+    }else if (action.type === "DECREMENT") {
+      return state - action.payload;
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div>
       <div className="container">
         <div className="row justify-content-center py-5 text-center">
           <div className="col-md-4">
             <div className="bg-white rounded shadow px-4 py-5">
-              <h5 className="text-center mb-4">Child: {count}</h5>
-              <button onClick={() => setCount(count - 1)} className='btn btn-warning mx-2'>Decrease</button>
-              <button onClick={() => setCount(prevCount => prevCount + 1)} className='btn btn-info mx-2'>Increase</button>
+              <h5 className="text-center mb-4">Child: {state}</h5>
+              <button onClick={() => dispatch({type: "DECREMENT", payload: 5})} className='btn btn-warning mx-2'>Decrease</button>
+              <button onClick={() => dispatch({type: "INCREMENT", payload: 5})} className='btn btn-info mx-2'>Increase</button>
             </div>
           </div>
         </div>
--- src/ShortForm.js
+++ src/ShortForm.js
@@ -1,15 +1,7 @@
-import React, { useReducer, useState } from 'react';
+import React, { useState } from 'react';
 
 const ShortForm = () => {
-  const initialState = 0;
-  const reducer = (state, action) => {
-    if(action.type === "INCREMENT"){
-      return state + 1;
-    }else if (action.type === "DECREMENT") {
-      return state -1;
-    }
-  }
-  const [state, dispatch] = useReducer(reducer, initialState)
+
 
   const [firstName, setFirstName] = useState('');
 
@@ -19,7 +11,7 @@ const ShortForm = () => {
   }
   return (
     <div>
-      {/* <div className="container">
+      <div className="container">
         <div className="row justify-content-center py-5 text-center mb-4">
           <div className="col-md-4">
             <div className="bg-white rounded shadow px-4 py-5">
@@ -32,17 +24,6 @@ const ShortForm = () => {
                   <button type="submit" className="btn btn-info">Submit</button>
                 </div>
               </form>
-            </div>
-          </div>
-        </div>
-      </div> */}
-            <div className="container">
-        <div className="row justify-content-center py-5 text-center">
-          <div className="col-md-4">
-            <div className="bg-white rounded shadow px-4 py-5">
-              <h5 className="text-center mb-4">Child: {state}</h5>
-              <button onClick={() => dispatch({type: "DECREMENT"})} className='btn btn-warning mx-2'>Decrease</button>
-              <button onClick={() => dispatch({type: "INCREMENT"})} className='btn btn-info mx-2'>Increase</button>
             </div>
           </div>
         </div>
