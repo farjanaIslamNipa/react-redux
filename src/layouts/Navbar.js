a8f4a9626163b26d52ba047f19b370534be1d170
farjanaIslamNipa <nipa.farzana87@gmail.com>
2022-12-13 23:40:23 +0600

    jyy

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
-      navbar
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <Link class="navbar-brand" to="/">Logo</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/about">About</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/top-rated">Top Rated</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/cart"><span className="text-info"><i class="fa-solid fa-cart-shopping"></i></span></Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </div>
   );
 };
